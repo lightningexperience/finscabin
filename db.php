af9d047cc35229d70313fceda4de8a7e08767d60
lightningexperience <lightningexperience@users.noreply.github.com>
2021-04-03 13:54:55 -0400

    Create db.php

--- db.php
+++ db.php
@@ -21,11 +21,11 @@ function Createdb(){
         $con = mysqli_connect($servername, $username, $password, $dbname);
 
         $sql = "
-                         CREATE TABLE IF NOT EXISTS leads(
-                           id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                           email VARCHAR (45),
-                           last_name VARCHAR (45) NOT NULL,
-                           first_name (45)
+                         CREATE TABLE IF NOT EXISTS books(
+                            id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                            book_name VARCHAR (25) NOT NULL,
+                            book_publisher VARCHAR (20),
+                            book_price FLOAT 
                         );
         ";
 
