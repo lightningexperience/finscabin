2c0f2e6bdf30cd997e17b5725e471aec33ddf22e
lightningexperience <lightningexperience@users.noreply.github.com>
2021-04-03 17:02:01 -0400

    Update index.php

--- index.php
+++ index.php
@@ -10,9 +10,12 @@ require_once ("operation.php");
 <title>Fins Cabin </title>
 </head>
 <body>
-    <div style="margin: 30px; margin-top: 50px;"> <center> <h1> Fins Cabin Workshop </h1> </div>  </center>
+    <div style="margin: 30px; margin-top: 50px;"> 
+        <center> <img src="wide-characters.jpg"> 
+        <h1>   Fins Cabin Workshop </h1> </div>  
+        </center>
      <center> <div style="margin: 30px; margin-top: 20px;"> 
- <img src="wide-characters.jpg">
+
  <p> Example usage of Salesforce Platform APIs: Update your own on-premise/cloud application by retrieving data from Salesforce application. </p>
  <p>Step 1: Use the your own program to connect to Salesforce Platform APIs (REST/SOAP). Get the data from Salesforce. </p>
  <p> Step 2: The from your program  connect to your own application and insert the data you got from Salesforce. </p>
