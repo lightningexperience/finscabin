fc446383564a238bea7c6f39f6e6b95958a99e50
lightningexperience <lightningexperience@users.noreply.github.com>
2021-04-04 01:07:23 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,24 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Fins Cabin </title>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport"
+    content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Fins Cabin</title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Custom stylesheet -->
+    <link rel="stylesheet" href="style.css">
+    
+    
 </head>
 <body>
     <div style="margin: 30px; margin-top: 50px;"> 
         <center> <img src="wide-characters.jpg"> 
-        <h1 class="display-4">   Fins Cabin Workshop </h1> </div>  
+        <h1 >   Fins Cabin Workshop </h1> </div>  
         </center>
    
      <center> <div style="margin: 30px; margin-top: 20px;"> 
