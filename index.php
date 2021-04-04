2bfb4930e5289a4b1ac1b92bcf93b8af86837b48
lightningexperience <lightningexperience@users.noreply.github.com>
2021-04-04 01:16:53 -0400

    Update index.php

--- index.php
+++ index.php
@@ -21,12 +21,13 @@
 <body>
     <div style="margin: 30px; margin-top: 50px;"> 
         <center> <img src="wide-characters.jpg"> 
-        <h1 >   Fins Cabin Workshop </h1> </div>  
+        <h1 >   Fins Cabin Workshop </h1> 
+        <h3> Event Driven Architecture in Financial Services <h3>   
+      </div>  
         </center>
-   
      <center> <div style="margin: 30px; margin-top: 20px;"> 
 
-   <a href="leads.php">Leads </a> &nbsp;&nbsp;&nbsp;<a href="customers.php">Customers</a> 
+   <a class="more" href="leads.php">Leads </a> &nbsp;&nbsp;&nbsp;<a class="more" href="customers.php">Customers</a> 
     <hr style="width:70%;">
          </div></center>
         
