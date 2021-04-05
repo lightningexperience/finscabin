8929fd020efdae75e5aeb4f66da5edb64de915f3
lightningexperience <lightningexperience@users.noreply.github.com>
2021-04-05 11:17:10 -0400

    Update db-customers.php

--- db-customers.php
+++ db-customers.php
@@ -1,1 +1,46 @@
 
+<?php
+
+function Createdb(){
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$dbname = ltrim($dbparts['path'],'/');
+    
+    
+
+    // create connection
+    $con = mysqli_connect($hostname, $username, $password, $dbname);
+
+    // Check Connection
+    if (!$con){
+        die("Connection Failed : " . mysqli_connect_error());
+    }
+
+    // create Database
+    $sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+
+    if(mysqli_query($con, $sql)){
+    $con = mysqli_connect($hostname, $username, $password, $dbname);
+
+        $sql = "             
+  CREATE TABLE IF NOT EXISTS customers (
+  id int,
+  accountid varchar(255),
+  name varchar(255),
+  address varchar(80));                                 
+        ";
+
+        if(mysqli_query($con, $sql)){
+            return $con;
+        }else{
+            echo "Cannot Create table...!";
+        }
+
+    }else{
+        echo "Error while creating database ". mysqli_error($con);
+    }
+
+}
